312ef6bfbec1c41513bd5447c448382506bfdc74
BlanchardIlela <blancilela@gmail.com>
2025-08-15 13:24:08 +0100

    pour le récupéré les apis depuis un autre fichier

---
+++ app.js
@@ -0,0 +1,14 @@
+import { fetchJSON } from "./functions/api.js";
+import { createElement } from "./functions/dom.js";
+
+try {
+    const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5');
+    
+} catch (e) {
+    const alertElement =  createElement('div', {
+        class: 'alert alert-danger m-2',
+        role: 'alert'
+    })
+    alertElement.innerText = 'Impossible de charger les éléments';
+    document.body.prepend(alertElement);
+}
