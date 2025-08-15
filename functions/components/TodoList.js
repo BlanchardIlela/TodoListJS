fe1a571352c61f57805e02c5be608518a01014f5
BlanchardIlela <blancilela@gmail.com>
2025-08-15 14:55:38 +0100

    le script pour ajouter une tâche

--- functions/components/TodoList.js
+++ functions/components/TodoList.js
@@ -8,8 +8,12 @@ import { createElement } from "../dom.js";
  */
 
 export class TodoList {
+
     /** @type {Todo[]} */
     #todos = [];
+
+     /** @type {HTMLUListElement[]} */
+    #listElement = [];
 
     /** @param {Todo[]} todos */
     constructor(todos) {
@@ -33,11 +37,33 @@ export class TodoList {
             </ul>
         </main>`
 
-        const list = element.querySelector('.list-group');
+        this.#listElement = element.querySelector('.list-group');
         for (const todo of this.#todos) {
             const t = new TodoListItem(todo);
-            t.appendTo(list);
+            this.#listElement.append(t.element);
         }
+        element.querySelector('form').addEventListener('submit', e => this.onSubmit(e) )
+    }
+
+    /**
+     * 
+     * @param {SubmitEvent} e 
+     */
+    onSubmit (e) {
+        e.preventDefault();
+        const form = e.currentTarget
+        const title = new FormData(form).get('title').toString().trim();
+        if(title === '') {
+            return
+        }
+        const todo = {
+            id: Date.now(),
+            title,
+            completed: false
+        }
+        const item = new TodoListItem(todo);
+        this.#listElement.prepend(item.element);
+        form.reset();
     }
 
 }
@@ -79,10 +105,10 @@ class TodoListItem {
     }
 
     /***
-     * @param {HTMLElement} element
+     * @param {HTMLElement}
      */
-    appendTo (element) {
-        element.append(this.#element)
+    get element () {
+        return this.#element
     }
 
     /**
