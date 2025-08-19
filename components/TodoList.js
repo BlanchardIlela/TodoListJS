300a053a0e92ebee32e86a35d66a68767d041fbf
BlanchardIlela <blancilela@gmail.com>
2025-08-19 07:48:52 +0100

    Mise en place d'un template html

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,4 +1,4 @@
-import { createElement } from "../functions/dom.js";
+import { cloneTemplate, createElement } from "../functions/dom.js";
 
 /**
  * @typedef {object} Todo
@@ -22,20 +22,23 @@ export class TodoList {
 
     /** @param {HTMLElement} element*/
     appendTo(element) {
-        element.innerHTML = `<form class="d-flex pb-4">
-            <input required="" class="form-control" type="text" placeholder="Acheter des patates..." name="title" data-com.bitwarden.browser.user-edited="yes">
-            <button class="btn btn-primary">Ajouter</button>
-        </form>
-        <main>
-            <div class="btn-group mb-4" role="group">
-                <button type="button" class=" btn btn-outline-primary active" data-filter="all">Toutes</button>
-                <button type="button" class=" btn btn-outline-primary" data-filter="todo">A faire</button>
-                <button type="button" class=" btn btn-outline-primary" data-filter="done">Faites</button>
-            </div>
+        element.append(
+            cloneTemplate('todolist-layout')
+        )
+        // element.innerHTML = `<form class="d-flex pb-4">
+        //     <input required="" class="form-control" type="text" placeholder="Acheter des patates..." name="title" data-com.bitwarden.browser.user-edited="yes">
+        //     <button class="btn btn-primary">Ajouter</button>
+        // </form>
+        // <main>
+        //     <div class="btn-group mb-4" role="group">
+        //         <button type="button" class=" btn btn-outline-primary active" data-filter="all">Toutes</button>
+        //         <button type="button" class=" btn btn-outline-primary" data-filter="todo">A faire</button>
+        //         <button type="button" class=" btn btn-outline-primary" data-filter="done">Faites</button>
+        //     </div>
 
-            <ul class="list-group">
-            </ul>
-        </main>`
+        //     <ul class="list-group">
+        //     </ul>
+        // </main>`
 
         this.#listElement = element.querySelector('.list-group');
         for (const todo of this.#todos) {
@@ -99,29 +102,20 @@ class TodoListItem {
     /** @type {Todo} */
     constructor (todo) {
         const id = `todo-${todo.id}`
-        const li = createElement('li', {
-            class: 'todo list-group-item d-flex align-items-center'
-        })
+        const li = cloneTemplate('todolist-item').firstElementChild
+        // const li = createElement('li', {
+        //     class: 'todo list-group-item d-flex align-items-center'
+        // })
         this.#element = li
-        const checkbox = createElement('input', {
-            type: 'checkbox',
-            class: 'form-check-input',
-            id: id,
-            checked: todo.completed ? '' : null
-        })
-        const label = createElement('label', {
-            class: 'ms-2 form-check-label',
-            for: id
-        })
+        const checkbox = li.querySelector('input')
+        checkbox.setAttribute('id', id)
+        if(todo.completed) {
+            checkbox.setAttribute('checked', '')
+        }
+        const label = li.querySelector('label')
+        label.setAttribute('for', id)
         label.innerText = todo.title
-        const button = createElement('button', {
-            class: 'ms-auto btn btn-danger btn-sm'
-        })
-        button.innerHTML = '<i class="bi-trash"></i>'
-
-        li.append(checkbox)
-        li.append(label)
-        li.append(button)
+        const button = li.querySelector('button')
         this.toggle(checkbox)
 
         //Création bouton supprimer la tâche
