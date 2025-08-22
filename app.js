8d714d3975648a2559f7761fd271f5c134392a47
BlanchardIlela <blancilela@gmail.com>
2025-08-22 07:24:03 +0100

    cache de l'api

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { createElement } from "./functions/dom.js";
 
 try {
     //const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5');
-    const todosInStorage = localStorage.getItem('todos')?.toString()
+    const todosInStorage = localStorage.getItem(('todos')?.toString())
     let todos = []
     if (todosInStorage) {
         todos = JSON.parse(todosInStorage)
